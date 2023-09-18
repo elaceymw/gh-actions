ddf250489368710e088822131b43f499ddfd3d35
Eric Lacey <elacey@mathworks.com>
2023-09-18 14:47:04 -0400

    comment out job

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,7 +6,7 @@ const exec = require('@actions/exec');
 function run() {
     // 1) Get some input values
     const bucket = core.getInput('bucket', { required: true });
-    const bucketRegion = core.getInput('bucket-region', { required: false });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
     const distFolder = core.getInput('dist-folder', { required: true });
 
     // 2) Upload files
